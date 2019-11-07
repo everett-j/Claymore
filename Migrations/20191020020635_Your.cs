b735a331391da4db65ddf95517141fe82c934127
Josh <jseverett16@gmail.com>
2019-11-07 13:06:14 -0500

    update to stats area

---
+++ Migrations/20191020020635_Your.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Claymore.Migrations
+{
+    public partial class Your : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostingCount",
+                table: "Postings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PostingCount",
+                table: "Postings",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
